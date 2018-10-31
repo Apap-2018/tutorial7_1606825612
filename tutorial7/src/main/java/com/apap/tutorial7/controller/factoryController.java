cd76cb56a5b4671943fcfa0ea0774466b7679424
zhelialifa <zhelialifa@gmail.com>
2018-10-31 19:56:00 +0700

    barangkali no 2nya bung

--- tutorial7/src/main/java/com/apap/tutorial7/controller/factoryController.java
+++ tutorial7/src/main/java/com/apap/tutorial7/controller/factoryController.java
@@ -7,14 +7,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.apap.tutorial7.rest.Setting;
 
 @RestController
-@RequestMapping("/model")
 public class factoryController {
 	
 	//tutorial7
@@ -26,8 +25,8 @@ public class factoryController {
 		return new RestTemplate();
 	}
 	
-	@GetMapping(value = "/factory/{namaFactory}")
-	 private ResponseEntity<String> getStatus(@PathVariable ("namaFactory") String factory) {
+	@GetMapping(value = "/model/{namaFactory}")
+	 private ResponseEntity<String> getStatus(@PathVariable ("namaFactory") String factory) throws Exception {
 	  String path = Setting.factoryUrl+"&factory="+factory+"&year="+ Calendar.getInstance().get(Calendar.YEAR);
 	  ResponseEntity<String> response = restTemplate.getForEntity(path, String.class);
 	  return response;
